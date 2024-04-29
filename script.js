bb5cfc96b767def400732e94349dabfa7425fc56
SarikViper <dzamolovsardor5@gmail.com>
2024-04-29 18:05:30 +0500

    script

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-let name =  'Alex';
-let money = 10000;
-let account = 7777;
-const first = prompt ('Как вас зовут?');
-if (first === name) {
-}
-else {
-    alert ('Пользователь не найден , досвидули ' +  first)
-}
-
-const second = prompt ('Номер счета?');
-if (second == account) {
-}
-else {
-    alert ('Пользователь не найден , досвидули '  + first)
-}
-const therd = prompt ('Сколько обналичить?');
-if (therd < money) {
-    alert ('Все отлично')
-}
-else {
-    alert ('Недостаточно средств')
-}
-
-
-     
-    
+///Первое задание
+let str = 'aaa@bbb@ccc';
+console.log(str.replaceAll('@', '!'));
+/// Второе задание
+const str2 = 'HELLOWORLD';
+const changing = str2.slice(0, 1).toUpperCase() + str2.slice(1).toLowerCase();
+console.log(changing);
+///Третье задание
+let str3 = 'Hello, it is HTML';
+console.log(str3.replace('HTML', 'not JS'));
+///Четвертое задание
+const str4 = 'alex';
+const changing2 = str4.slice(0, 1).toUpperCase() + str4.slice(1);
+console.log(changing2);
+///Пятое задание
+const str5 = Math.round(Math.random() * (1000 - 1 + 1) + 1);
+console.log(str5);
+///Шестое задание 
+let str6 = 'Lex Lutter has a big suit'
+console.log(str6.replace('Lex Lutter has a big suit', 'ALex'));
